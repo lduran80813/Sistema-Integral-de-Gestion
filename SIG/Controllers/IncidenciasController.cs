3649c9d466b24152b128bfb56bb37e116cb146ed
BryanOR96 <bryorozco@gmail.com>
2024-08-11 20:13:09 -0600

    modulos

---
+++ SIG/Controllers/IncidenciasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIG.Controllers
+{
+    public class IncidenciasController : Controller
+    {
+        // GET: Incidencias
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIG/Controllers/Plan_AccionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIG.Controllers
+{
+    public class Plan_AccionController : Controller
+    {
+        // GET: Plan_Accion
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
