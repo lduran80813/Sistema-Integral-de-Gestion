5d20ca5311956b5549fa9c54106473058f3559c3
ldiegoduran <105762258+ldiegoduran@users.noreply.github.com>
2024-12-17 16:14:34 -0600

    Lógica de capacitacion empleados

--- SIG/Controllers/EmpleadosController.cs
+++ SIG/Controllers/EmpleadosController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SIG.Entidades;
+using SIG.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,116 @@ namespace SIG.Controllers
 {
     public class EmpleadosController : Controller
     {
+        private readonly EmpleadosModel _empleadosModel;
+
+        public EmpleadosController()
+        {
+            _empleadosModel = new EmpleadosModel();
+        }
+
+
         // GET: Empleados
         public ActionResult Registro()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult ListarCapacitaciones()
+        {
+            var planes = _empleadosModel.ListarCapacitaciones();
+            return View(planes);
+        }
+
+        // Acción GET para registrar un nuevo Plan de Acción
+        [HttpGet]
+        public ActionResult RegistroCapacitacion()
+        {
+            var empleados = _empleadosModel.ListarEmpleados();
+            ViewBag.Empleados = empleados ?? new List<SelectListItem>();
+
+
+            var capacitacion = new EmpCapacitacion
+            {
+                Participantes = new List<EmpCapacitacionParticipantes> { new EmpCapacitacionParticipantes() }
+            };
+
+            return View(capacitacion);
+        }
+
+        // Acción POST para registrar un nuevo Plan de Acción
+        [HttpPost]
+        public JsonResult RegistroAjax(EmpCapacitacion capacitacion)
+        {
+            if (ModelState.IsValid)
+            {
+                bool resultado = _empleadosModel.InsertarCapacitacion(capacitacion);
+
+                if (resultado)
+                {
+                    return Json(new { success = true, message = "Plan de Acción registrado con éxito." });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Hubo un problema al registrar el Plan de Acción." });
+                }
+            }
+            return Json(new { success = false, message = "Datos proporcionados no válidos." });
+        }
+
+        [HttpGet]
+        public ActionResult VerParticipantes(int idCapacitacion)
+        {
+            var capacitacion = _empleadosModel.VerCapacitacion(idCapacitacion);
+            if (capacitacion == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Capacitacion = capacitacion;
+            return View(capacitacion.Participantes);
+        }
+
+        [HttpGet]
+        public ActionResult EditarCapacitacion(int idCapacitacion)
+        {
+            var plan = _empleadosModel.VerCapacitacion(idCapacitacion);
+            if (plan == null)
+            {
+                return HttpNotFound();
+            }
+
+            var empleados = _empleadosModel.ListarEmpleados();
+            ViewBag.Empleados = empleados ?? new List<SelectListItem>();
+
+            return View(plan);
+        }
+
+        // Acción POST para editar un Plan de Acción
+        [HttpPost]
+        public ActionResult EditarCapacitacion(EmpCapacitacion capacitacion)
+        {
+            if (ModelState.IsValid)
+            {
+                bool resultado = _empleadosModel.ActualizarCapacitacion(capacitacion);
+                if (resultado)
+                {
+                    TempData["Message"] = "Capacitación actualizada con éxito.";
+                    return RedirectToAction("ListarCapacitaciones");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hubo un problema al actualizar la capacitación.");
+                }
+            }
+
+            var empleados = _empleadosModel.ListarEmpleados();
+            ViewBag.Empleados = empleados ?? new List<SelectListItem>();
+
+            return View(capacitacion);
+        }
+
+
     }
 }
---
+++ SIG/Models/EmpleadosModel.cs
@@ -0,0 +1,187 @@
+﻿using SIG.BaseDatos;
+using SIG.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIG.Models
+{
+    public class EmpleadosModel
+    {
+
+        public List<Entidades.EmpCapacitacion> ListarCapacitaciones()
+        {
+            using (var context = new SistemaIntegralGestionEntities())
+            {
+                return (from x in context.Emp_Capacitacion
+                        select new Entidades.EmpCapacitacion
+                        {
+                            id = x.id,
+                            nombre_capacitacion = x.nombre_capacitacion,
+                            descripcion_capacitacion = x.descripcion_capacitacion,
+                            fecha_inicio = (DateTime)x.fecha_inicio,
+                            fecha_finalizacion = x.fecha_finalizacion,
+                            ResponsableId = x.ResponsableId ?? 0
+                        }).ToList();
+            }
+        }
+
+        public IEnumerable<SelectListItem> ListarEmpleados()
+        {
+            using (var context = new SistemaIntegralGestionEntities())
+            {
+                var empleados = context.Empleado
+                                       .Select(e => new
+                                       {
+                                           e.id,
+                                           e.nombre,
+                                           e.apellidos
+                                       })
+                                       .ToList();
+
+                var empleadosList = empleados.Select(e => new SelectListItem
+                {
+                    Value = e.id.ToString(),
+                    Text = $"{e.nombre} {e.apellidos}"
+                }).ToList();
+
+                return empleadosList;
+            }
+        }
+
+        public bool InsertarCapacitacion(Entidades.EmpCapacitacion capacitacion)
+        {
+            var rowsAffected = 0;
+
+            using (var context = new SistemaIntegralGestionEntities())
+            {
+                int idSesion = int.Parse(HttpContext.Current.Session["IdUsuario"].ToString());
+
+                var tCapacitacion = new SIG.BaseDatos.Emp_Capacitacion
+                {
+                    id = idSesion,
+                    nombre_capacitacion = capacitacion.nombre_capacitacion,
+                    descripcion_capacitacion = capacitacion.descripcion_capacitacion,
+                    fecha_inicio = capacitacion.fecha_inicio,
+                    fecha_finalizacion = capacitacion.fecha_finalizacion,
+                    ResponsableId = capacitacion.ResponsableId
+                };
+
+                context.Emp_Capacitacion.Add(tCapacitacion);
+                rowsAffected = context.SaveChanges();
+
+                if (rowsAffected > 0 && capacitacion.Participantes != null)
+                {
+                    foreach (var participante in capacitacion.Participantes)
+                    {
+                        var tParticipante = new SIG.BaseDatos.Emp_Capacitacion_Participantes
+                        {
+                            participante_id = participante.participante_id,
+                            capacitacion_id = tCapacitacion.id
+                        };
+
+                        context.Emp_Capacitacion_Participantes.Add(tParticipante);
+                    }
+
+                    rowsAffected = context.SaveChanges();
+                }
+            }
+
+            return rowsAffected > 0;
+        }
+
+        public Entidades.EmpCapacitacion VerCapacitacion(int idCapacitacion)
+        {
+            using (var context = new SistemaIntegralGestionEntities())
+            {
+
+                var capacitacion = (from x in context.Emp_Capacitacion
+                            where x.id == idCapacitacion
+                            select new Entidades.EmpCapacitacion
+                            {
+                                id = x.id,
+                                nombre_capacitacion = x.nombre_capacitacion,
+                                descripcion_capacitacion = x.descripcion_capacitacion,
+                                fecha_inicio = x.fecha_inicio,
+                                fecha_finalizacion = x.fecha_finalizacion,
+                                ResponsableId = x.ResponsableId ?? 0
+                            }).FirstOrDefault();
+
+                if (capacitacion == null)
+                {
+                    return null;
+                }
+
+                var participantes = (from t in context.Emp_Capacitacion_Participantes
+                              where t.capacitacion_id == idCapacitacion
+                              select new
+                              {
+                                  t.id,
+                                  t.capacitacion_id,
+                                  t.participante_id,
+                                  t.calificacion,
+                                  t.retroalimentacion
+                              }).ToList();
+
+                capacitacion.Participantes = participantes.Select(t => new EmpCapacitacionParticipantes
+                {
+                    id = t.id,
+                    capacitacion_id = t.capacitacion_id,
+                    participante_id = t.participante_id,
+                    calificacion = t.calificacion,
+                    retroalimentacion = t.retroalimentacion
+                }).ToList();
+
+                var participantesIds = capacitacion.Participantes.Select(t => t.participante_id).Distinct().ToList();
+
+                var empleados = context.Empleado
+                    .Where(e => participantesIds.Contains(e.id))
+                    .ToList();
+
+                foreach (var partipante in capacitacion.Participantes)
+                {
+                    var empleado = empleados.FirstOrDefault(e => e.id == partipante.participante_id);
+                    if (empleado != null)
+                    {
+                        partipante.Participante = new PDAEmpleado
+                        {
+                            Id = empleado.id,
+                            Nombre = $"{empleado.nombre} {empleado.apellidos}"
+                        };
+                    }
+
+                }
+
+                return capacitacion;
+            }
+        }
+
+        // Actualizar un Plan de Acción existente
+        public bool ActualizarCapacitacion(Entidades.EmpCapacitacion capacitacion)
+        {
+            var rowsAffected = 0;
+
+            using (var context = new SistemaIntegralGestionEntities())
+            {
+
+                var capacitacionExistente = context.Emp_Capacitacion.FirstOrDefault(p => p.id == capacitacion.id);
+                if (capacitacionExistente != null)
+                {
+
+                    capacitacionExistente.nombre_capacitacion = capacitacion.nombre_capacitacion;
+                    capacitacionExistente.descripcion_capacitacion = capacitacion.descripcion_capacitacion;
+                    capacitacionExistente.fecha_inicio = capacitacion.fecha_inicio;
+                    capacitacionExistente.fecha_finalizacion = capacitacion.fecha_finalizacion;
+                    capacitacionExistente.ResponsableId = capacitacion.ResponsableId;
+
+                    rowsAffected = context.SaveChanges();
+                }
+            }
+
+            return rowsAffected > 0;
+        }
+
+    }
+}
